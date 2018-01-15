7dec34050d61a4b0105147c3095124addd596db1
ericcalabretta <eric.calabretta@gmail.com>
2018-01-15 15:15:56 -0600

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# cookbook_pipeline
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: cookbook_pipeline
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
